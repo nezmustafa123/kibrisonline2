4ead60e87a6153a2be872f52051faee78f6bf7b7
nezmustafa123 <nezmustafa123@gmail.com>
2019-05-17 14:34:05 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".trigger").on('click', function(e) {
+  $('.title').not($(this)).removeClass('open');
+  $(this).toggleClass('open');
+  $('.content').not($(this).next('.content')).slideUp();
+  $(this).next('.content').slideToggle();
+});
+  });
+
+
+
